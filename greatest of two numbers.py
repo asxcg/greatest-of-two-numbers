42b776f65722713bb6e72a47f3d976cbb25cb366
ELECTRONIC VOLG <96625925+asxcg@users.noreply.github.com>
2022-09-29 21:51:51 +0530

    Add files via upload

---
+++ greatest of two numbers.py
@@ -0,0 +1,10 @@
+#python
+#py greatest of two numbers
+n1=int(input("ENTER THE N1:"))
+n2=int(input("ENTER THE N2:"))
+if (n1>n2):
+    print(n1,"is greatest")
+elif (n1<n2):
+    print(n2,"is greatest")
+else:
+    print(n1,"is equal to",n2)
